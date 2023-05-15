5b7a2ab055219434fc4e812b24363270e85eaaa0
S.M.Mehrabul Islam <smmehrabul-2017614964@cs.du.ac.bd>
2023-05-16 01:47:00 +0600

    deploy debug

--- medimeet/settings.py
+++ medimeet/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = 'RENDER' not in os.environ
-DEBUG = False
+DEBUG = True
 PRODUCTION = True
 APPEND_SLASH = True
 
